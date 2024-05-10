fe85f401e3a82828014f05d35abd529c34f8fc91
Urias Pereira <uriasluis64@gmail.com>
2024-05-10 14:59:24 -0300

    estrutura home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,39 @@
-import { styled } from '@/styles/index'
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import { HomeContainer, Product } from '@/styles/pages/home';
+import Image from 'next/image';
+import shirt1 from '@/assets/Shirt/1.png'
+import shirt2 from '@/assets/Shirt/2.png'
+import shirt3 from '@/assets/Shirt/3.png'
 
 export default function Home() {
   return (
-    <Button>
-      <span>teste</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      {/* <Product>
+        <Image src={shirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Z</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product> */}
+    </HomeContainer>
   );
 }
